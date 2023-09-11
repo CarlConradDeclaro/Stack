540e9b917792e2f9c753bbe47a8e2ea296b6b917
Carl Conrad C. Declaro <110441309+CarlConradDeclaro@users.noreply.github.com>
2023-09-11 02:06:54 +0100

    Add files via upload

---
+++ BuiltInStack.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datastructure;
+import java.util.Stack;
+/**
+ *
+ * @author carlconrad
+ */
+public class BuiltInStack {
+
+    public static void main(String[] args) {
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        
+        
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+        
+        
+    }
+}
